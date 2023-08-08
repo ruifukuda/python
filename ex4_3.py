811fb81c9b271ec83294eb4b91d18155f39d6639
ruifukuda <rufk222@gmail.com>
2023-08-08 11:21:13 +0900

    第4章まで'

---
+++ ex4_3.py
@@ -0,0 +1,14 @@
+for num in range(10):
+    print(f'{10 - num}, ', end = '')
+print('Lift off !')
+
+for i in range(10, 0, -1):
+    print(f'{i}', end = ', ')
+print('Lift off !')
+
+for i in range(3, 10, 2):
+    print(f'{i}', end = ', ')
+print('Lift off !')
+
+# 第2引数で第1から第2前まで
+# 第3引数で第1から第2前まで第3の値ずつスキップ
